df414969e7bab07a4dde5acd622cac68778b7daa
banu-i <152266442+banu-i@users.noreply.github.com>
2024-08-27 17:20:02 +0200

    20240827 courseAssignment Update
    index.js - update;
    express app gestartet

--- index.js
+++ index.js
@@ -1,21 +1,8 @@
-const todoSequelize = require("./src/database/setup/database");
-const app = require("./src/server");
-const serverless = require("serverless-http");
+const express = require("express");
+const app = express();
 
-// Zugriff auf Umgebungsvariablen
-// const { PORT } = process.env;
-const PORT = process.env.PORT;
-
-todoSequelize
-  .sync()
-  .then(() => {
-    console.log("DB has been successfully initialized");
-  })
-  .catch((e) => {
-    console.log(e);
-  });
-
-// App hört im folgenden auf den Port, welcher über die Umgebungsvariable definiert ist
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`);
+app.get("/", function (req, res) {
+  res.send("Hello World");
 });
+
+app.listen(3000);
