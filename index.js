a176082f308815cd939db3a5918e7933d5a8f635
banu-i <152266442+banu-i@users.noreply.github.com>
2024-08-28 14:42:32 +0200

    cleaning my repo

--- index.js
+++ index.js
@@ -5,4 +5,8 @@ app.get("/", function (req, res) {
   res.send("Hello World");
 });
 
+app.get("/user", function (req, res) {
+  res.send("Hello User");
+});
+
 app.listen(3000);
--- src/middleware/authMiddleware.js
+++
@@ -1,7 +0,0 @@
-function authMiddleware(req, res, next) {
-  // Authentifizierungslogik
-  console.log("Authentifizierungsmiddleware aktiviert");
-  next();
-}
-
-module.exports = authMiddleware;
