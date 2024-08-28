e34ec85e9a0bc0d53a0035c2c25e5119e6eca6ac
banu-i <152266442+banu-i@users.noreply.github.com>
2024-08-28 16:26:22 +0200

    20240828 courseAssignment - extra
    Hier die ersten Schritt für Schritt Themen, die du in der Express App umsetzen sollst:
    1.Füge weitere GET Routen Hinzu und Teste diese über Postman
     2. Füge PUT POST DELETE Anfragen in deine App mit ein und teste diese über Postman
    3. Versuche query Parameter an die GET Anfragen anzuhängen und diese innerhalb von deiner  Express App auszulesen
    4. Versuche einen Body an deine PUT POST und DELETE Anfragen anzuhängen
    
    Die gesamte Aufgabe kannst du in der index.js Datei abarbeiten

--- index.js
+++ index.js
@@ -1,12 +1,52 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
+// Get Routen
 app.get("/", function (req, res) {
   res.send("Hello World");
 });
 
 app.get("/user", function (req, res) {
   res.send("Hello User");
+});
+
+app.get("/users", function (req, res) {
+  res.send("All Users - hello");
+});
+
+app.get("/entries", function (req, res) {
+  res.send("All Entries");
+});
+
+app.get("/images", function (req, res) {
+  res.send("Overview of all Images");
+});
+
+app.get("/friends", function (req, res) {
+  res.send("Overview of all friends");
+});
+
+// Post Route
+app.post("/user", function (req, res) {
+  const newUser = req.body;
+  res.send(`User created: ${JSON.stringify(newUser)}`);
+});
+
+// Put Route
+app.put("/user:id", function (req, res) {
+  const userId = req.params.id;
+  const updatedUser = req.body;
+  res.send(
+    `User with ID ${userId} updated with data: ${JSON.stringify(updatedUser)}`
+  );
+});
+
+// Delete Route
+app.delete("/user:id", function (req, res) {
+  const userId = req.params.id;
+  res.send(`User with ID ${userId} deleted`);
 });
 
 app.listen(3000);
