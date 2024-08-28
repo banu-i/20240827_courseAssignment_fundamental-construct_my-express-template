afca983cf8c5f7ea9a8d19b4f97664dfdab91dc9
banu-i <152266442+banu-i@users.noreply.github.com>
2024-08-28 17:15:05 +0200

    20240828 courseAssignment - extra (5)
    5. res.send mit res.json austauschen --> Dazu Beispiele raussuchen vorher und nachgucken, wie re.json aufgebaut wird und warum das verwendet wird

--- index.js
+++ index.js
@@ -5,48 +5,46 @@ app.use(express.json());
 
 // Get Routen
 app.get("/", function (req, res) {
-  res.send("Hello World");
+  res.json("Hello World");
 });
 
 app.get("/user", function (req, res) {
-  res.send("Hello User");
+  res.json("Hello User");
 });
 
 app.get("/users", function (req, res) {
-  res.send("All Users - hello");
+  res.json("All Users - hello");
 });
 
 app.get("/entries", function (req, res) {
-  res.send("All Entries");
+  res.json("All Entries");
 });
 
 app.get("/images", function (req, res) {
-  res.send("Overview of all Images");
+  res.json("Overview of all Images");
 });
 
 app.get("/friends", function (req, res) {
-  res.send("Overview of all friends");
+  res.json("Overview of all friends");
 });
 
 // Post Route
 app.post("/user", function (req, res) {
   const newUser = req.body;
-  res.send(`User created: ${JSON.stringify(newUser)}`);
+  res.json({ message: "User created", user: newUser });
 });
 
 // Put Route
 app.put("/user:id", function (req, res) {
   const userId = req.params.id;
   const updatedUser = req.body;
-  res.send(
-    `User with ID ${userId} updated with data: ${JSON.stringify(updatedUser)}`
-  );
+  res.json({ message: `User with ID ${userId} updated`, updatedUser });
 });
 
 // Delete Route
 app.delete("/user:id", function (req, res) {
   const userId = req.params.id;
-  res.send(`User with ID ${userId} deleted`);
+  res.json(`User with ID ${userId} deleted`);
 });
 
 app.listen(3000);
