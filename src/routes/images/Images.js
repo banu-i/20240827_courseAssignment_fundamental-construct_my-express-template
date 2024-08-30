029d8656582996e4e030edea5eeca96eb12f821c
banu-i <152266442+banu-i@users.noreply.github.com>
2024-08-30 14:08:07 +0200

    20240828- courseAssignment extra

---
+++ src/routes/images/Images.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+// Get Routen
+app.get("/", function (req, res) {
+  res.json("Hello World");
+});
+
+app.get("/user", function (req, res) {
+  res.json("Hello User");
+});
+
+app.get("/users", function (req, res) {
+  res.json("All Users - hello");
+});
+
+app.get("/entries", function (req, res) {
+  res.json("All Entries");
+});
+
+app.get("/images", function (req, res) {
+  res.json("Overview of all Images");
+});
+
+app.get("/friends", function (req, res) {
+  res.json("Overview of all friends");
+});
+
+// Post Route
+app.post("/user", function (req, res) {
+  const newUser = req.body;
+  res.json({ message: "User created", user: newUser });
+});
+
+// Put Route
+app.put("/user:id", function (req, res) {
+  const userId = req.params.id;
+  const updatedUser = req.body;
+  res.json({ message: `User with ID ${userId} updated`, updatedUser });
+});
+
+// Delete Route
+app.delete("/user:id", function (req, res) {
+  const userId = req.params.id;
+  res.json(`User with ID ${userId} deleted`);
+});
+
+app.listen(3000);
